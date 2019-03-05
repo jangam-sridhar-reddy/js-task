d35890ede30d9e6a715fcc8f2bc8e28de43c6ccd
jangam-sridhar-reddy <48112414+jangam-sridhar-reddy@users.noreply.github.com>
2019-03-05 19:48:58 +0530

    commit from office on 5 - 3 -2019

--- all-task.js
+++ all-task.js
@@ -247,13 +247,13 @@ function tipCalculator(money){
     var percentage;
     switch(true){
         case (money < 50):
-            percentage = 0.2;
+            percentage = 0.2; // 20% => 20 / 100 = 0.2
             break;
         case (money >= 50 && money < 200):
-            percentage =  0.15;
+            percentage =  0.15; // 15% => 15 / 100 = 0.15
             break;
         case (money > 200):
-            percentage =  0.1;
+            percentage =  0.1; // 10% => 10 / 100 = 0.1
             break;
         
     }   
@@ -278,9 +278,13 @@ var sridhar = {
     birthYear : 1992,
     family : ["Indiramma", "Preethi", "Laxmamma"],
     job : "Teacher",
-    isMarried : false
+    isMarried : false,
+    calAge : function(){
+        this.age = 2019 - this.birthYear;
+    }
 };
 
+sridhar.calAge();
 console.log(sridhar);
 console.log(sridhar.firstName);
 console.log(sridhar["job"]);
@@ -306,4 +310,313 @@ kondal.isMarried = false;
 kondal["job"] =  "He Works In AirPort ";
 
 console.log(kondal);
+
+
+var john = {
+    
+    fullName : "John Smith",
+    mass : 98,
+    height : 1.92,
+    calBMI : function(){
+        this.BMI = this.mass / (this.height * this.height);
+        return this.BMI;
+    }
+};
+
+
+var mark = {
+    
+    fullName : "Mark Smith",
+    mass : 98,
+    height : 1.92,
+    calBMI : function(){
+        this.BMI = this.mass / (this.height * this.height);
+        return this.BMI;
+    }
+};
+john.calBMI();
+mark.calBMI();
+console.log(john, mark);
+
+(john.BMI > mark.BMI) ? console.log(john.fullName + " has Higher BMI Value with " + john.BMI) : console.log(mark.fullName + " has Higher BMI Value with " + mark.BMI);
+
+
+switch(true){
+    case (john.BMI > mark.BMI) :
+        console.log(john.fullName + " has Higher BMI Value with " + john.BMI);
+        break;
+    case (john.BMI < mark.BMI) :
+        console.log(mark.fullName + " has Higher BMI Value with " + mark.BMI);
+        break;    
+    default:
+        console.log(john.fullName + " And " + mark.fullName + " has Same BMI Value with " + john.BMI + " = " + mark.BMI);
+        break;    
+}
+
+if (john.BMI > mark.BMI) {
+    console.log(john.fullName + " has Higher BMI Value with " + john.BMI);
+}else if (john.BMI < mark.BMI) {
+    console.log(mark.fullName + " has Higher BMI Value with " + mark.BMI);
+}
+else{
+    console.log(john.fullName + " And " + mark.fullName + " has Same BMI Value with " + john.BMI + " = " + mark.BMI);
+}
+
+
+//loop and iteration
+
+
+//forward for loop
+
+for (var  i = 0; i < 10; i++){
+    console.log(i);
+}
+
+//backward for loop
+
+for (var i = 10 - 1; i >= 0; i--){
+    console.log(i);
+}
+
+
+// forward while loop
+var i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+//backword while loop
+var i = 10 - 1;
+while(i >= 0 ){
+    console.log(i);
+    i--;
+}
+
+//display two table in farward using forloop (2 x 1 = 2)
+
+for (var i = 1; i <= 10; i++){
+    console.log("2 x " + i + " = " + (i + i));
+}
+
+//display two table in backward using forloop (2 x 1 = 2)
+
+for (var i = 10 ; i >= 1; i--){
+    console.log("2 x " + i + " = " + (i + i));
+}
+
+//display two table in farward using whileloop (2 x 1 = 2)
+
+
+var i = 1;
+while (i <= 10){
+    console.log("2 x " + i + " = " + (i + i));
+    i++;
+}
+
+//display two table in backward using whileloop (2 x 1 = 2)
+
+var i = 10;
+while (i >= 1) {
+    console.log("2 x " + i + " = " + (i + i));
+    i--;
+}
+
+var sridharArray = ["Sridhar", "Reddy", 1992, "Indiramma", "Preethi", "Designer"];
+
+//printing arraylist in forward using forloop
+
+for (var i = 0; i < sridharArray.length; i++){
+    console.log(sridharArray[i]);
+}
+
+//printing array list in backward using forloop
+
+for (var i = sridharArray.length - 1; i >= 0; i--){
+    console.log(sridharArray[i]);
+}
+
+
+//printing arraylist in forward using while loop
+
+
+var i = 0;
+while(i < sridharArray.length){
+    console.log(sridharArray[i]);
+    i++;
+}
+
+//printing arraylist in backward using while loop
+
+
+var i = sridharArray.length - 1;
+while(i >= 0){
+    console.log(sridharArray[i]);
+    i--;
+}
+
+// using continue and break statements  in for loop print arraylist in farward
+
+for (var i = 0; i < sridharArray.length; i++){
+    if(typeof(sridharArray[i]) !== "string"){
+        continue;
+    }
+    console.log(sridharArray[i]);
+    
+}
+for ( var i = 0; i < sridharArray.length; i++){
+    if(typeof(sridharArray[i]) !== "string"){
+        break;
+    }    
+    console.log(sridharArray[i]);
+}
+
+// using continue and break statements  in for loop print arraylist in backward
+
+for (var i = sridharArray.length - 1; i >= 0; i--){
+    
+    if(typeof(sridharArray[i]) !== "string"){
+        continue;
+    }
+    console.log(sridharArray[i]);
+    
+}
+for ( var i = sridharArray.length - 1; i >= 0; i--){
+    if(typeof(sridharArray[i]) !== "string"){
+        break;
+    }    
+    console.log(sridharArray[i]);
+}
+
+// using continue and break statements  in while loop print arraylist in farward
+
+
+var i = 0;
+
+while( i < sridharArray.length){
+   
+    if(typeof(sridharArray[i]) !== "string"){
+        i++;
+        continue;
+    }
+    console.log(sridharArray[i]);
+    i++;
+    
+   
+}
+
+
+var i =0;
+while (i < sridharArray.length){
+  
+    if(typeof(sridharArray[i]) !== "string"){
+      
+        break;
+    }
+    console.log(sridharArray[i]);
+    i++;
+} 
+
+// using continue and break statements  in while loop print arraylist in backward
+
+
+var i = sridharArray.length - 1;
+while(i >= 0){
+    if (typeof(sridharArray[i]) !== "string"){
+        i--;
+        continue;
+    }
+    console.log(sridharArray[i]);
+    i--;
+} 
+
+
+var i = sridharArray.length - 1;
+while (i >= 0){
+    if(typeof(sridharArray[i]) !== "string"){
+        break;
+    }
+    console.log(sridharArray[i]);
+    i--;
+}
+
+
+var johnFamily = {
+    firstName: "John Smith",
+    bills : [124, 48, 268, 180, 42],
+    allTips : [],
+    finalBills : [],
+    calculatTips : function(){
+        for(var i = 0; i < this.bills.length; i++){
+           
+            switch(true){
+                case (this.bills[i] < 50):
+                    this.allTips.push(this.bills[i] * 0.2);
+                    this.finalBills.push(this.bills[i]  + this.allTips[i]);
+                    break;
+                case (this.bills[i] >= 50 && this.bills[i] < 200):
+                    this.allTips.push(this.bills[i] * 0.15);
+                    this.finalBills.push(this.bills[i]  + this.allTips[i]);
+                    break;
+                case (this.bills[i] >= 200):
+                    this.allTips.push(this.bills[i] * 0.1);
+                    this.finalBills.push(this.bills[i]  + this.allTips[i]);
+                    break;        
+            }
+            console.log(this.allTips[i], this.finalBills[i]);
+        }    
+    }
+    
+
+};
+johnFamily.calculatTips() ;
+console.log(johnFamily);
+
+var markFamily = {
+    firstName: "Mark Miller",
+    bills : [77, 375, 110, 45],
+    allTips : [],
+    finalBills : [],
+    calculatTips : function(){
+        for (var i = 0; i < this.bills.length; i++){
+            
+            if(this.bills[i] < 100){
+                this.allTips.push(this.bills[i] * 0.2);
+                this.finalBills.push(this.bills[i] + this.allTips[i]);
+                
+            }else if (this.bills[i] >= 100 && this.bills[i] < 300){
+                this.allTips.push(this.bills[i] * 0.1);
+                this.finalBills.push(this.bills[i] + this.allTips[i]);
+            }else if(this.bills[i] >= 300){
+                this.allTips.push(this.bills[i] * 0.25);
+                this.finalBills.push(this.bills[i] + this.allTips[i]);
+            }
+            console.log(this.allTips[i], this.finalBills[i]);
+        }
+    }
+};
+
+function avgTipsCalculator(tips){
+   
+    sum = 0;
+    for (var i = 0; i < tips.length; i++){
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+markFamily.calculatTips();
+console.log(markFamily);
+
+johnFamily.avg = avgTipsCalculator(johnFamily.allTips);
+markFamily.avg = avgTipsCalculator(markFamily.allTips);
+console.log(johnFamily, markFamily);
+console.log(johnFamily.avg, markFamily.avg);
+
+if(johnFamily.avg > markFamily.avg){
+    console.log(johnFamily.firstName + ' Has Paid the Higher Tips Than ' + markFamily.firstName + ' Family');
+}else if(johnFamily.avg < markFamily.avg){
+    console.log(markFamily.firstName + ' Has Paid the Higher Tips Than ' + johnFamily.firstName + ' Family');
+}else {
+    console.log('they paid equal tips on an Avg');
+}
 
